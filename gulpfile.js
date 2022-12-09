e13969b144fc1e1a6b655edba44494dc15a84dfa
rusinko23 <rusinko200323@gmail.com>
2022-12-09 10:13:56 +0100

    added header-top and popular categories, and connected mixitup

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,8 @@ function styles () {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
-    'app/js/main.js'
+    'app/js/main.js',
+    'node_modules/mixitup/dist/mixitup.js'
   ])
   .pipe(concat('main.min.js'))
   .pipe(uglify())
